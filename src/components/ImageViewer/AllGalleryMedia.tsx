078e3cf343a9ee9a86bf9df4b583d9466a808227
Shivachii <videllkonya@gmail.com>
2025-07-16 21:52:37 +0300

    testing images sanity problem

--- src/components/ImageViewer/AllGalleryMedia.tsx
+++ src/components/ImageViewer/AllGalleryMedia.tsx
@@ -3,7 +3,6 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import { urlFor } from "../../../sanity/lib/image";
 
 interface Gallery {
   title: string;
